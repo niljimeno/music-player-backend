6e482eb1a12fbd0ca41e7c54204437218a02ead7
niljimeno <niliara@proton.me>
2026-01-21 13:09:03 +0100

    remove song files after download

--- src/music_player_backend/routes/song.clj
+++ src/music_player_backend/routes/song.clj
@@ -2,7 +2,8 @@
   (:require [music-player-backend.server :as server]
             [music-player-backend.json :as json]
             [music-player-backend.yt-dlp :as yt-dlp]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.java.shell :as sh]))
 
 (defn get-filename
   [dlp-output]
@@ -13,17 +14,26 @@
        (drop 1)
        (apply str)))
 
+(defn octet-headers
+  [filepath]
+  {"content-type" "application/octet-stream"
+   "content-disposition" (str "attachment; filename=\""
+                              filepath
+                              "\"")})
+
 (defn route
   "Route to download a song"
   [req]
   (let [url (json/value-from-request :url req)
         filename (get-filename (yt-dlp/download-song "resources/%(title)s.%(ext)s" url))
-        filepath (str "resources/" filename)]
-    (server/respond
-     (-> filepath
-         io/file
-         io/input-stream)
-     :headers {"content-type" "application/octet-stream"
-               "content-disposition" (str "attachment; filename=\""
-                                          filepath
-                                          "\"")})))
+        filepath (str "resources/" filename)
+        response (server/respond
+                  (-> filepath
+                      io/file
+                      io/input-stream)
+                  :headers (octet-headers filepath))]
+    (sh/sh "rm"
+           "-f"
+           filepath)
+
+    response))
