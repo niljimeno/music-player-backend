0ee35895378046ccbb133641b997b483afaecbe8
niljimeno <niliara@proton.me>
2026-01-23 13:05:49 +0100

    make yt-dlp agnostic (needs cleaning)

--- src/music_player_backend/routes/song.clj
+++ src/music_player_backend/routes/song.clj
@@ -5,14 +5,46 @@
             [clojure.java.shell :as sh]
             [clojure.java.io :as io]))
 
-(defn get-filename
+(defn get-output-names
   [dlp-output]
-  (->> (re-seq #"\[ExtractAudio\] Destination: resources\/(.*)"
-               dlp-output)
-       first first
-       (drop-while #(not (= % \/)))
-       (drop 1)
-       (apply str)))
+  ;;
+  (let [audio (->> (re-seq #"\[ExtractAudio\] (.*)"
+                           dlp-output)
+                   first first
+                   (drop-while #(not (= % \/)))
+                   (drop 1) ;; drop the /
+                   (#(do (println "Jodete alvaro" %)
+                         %))
+                   (apply str))
+        thumbnail (do (println "Working with" dlp-output)
+                      (->> (re-seq #"\[info\] Writing video thumbnail(.*)"
+                                   dlp-output)
+                           (#(do (println "Got" (drop-while (fn [x] (not (= x \/))) (first (first %))))
+                                 %))
+                           first first
+                           (drop-while #(not (= % \/)))
+                           (drop 1) ;; drop the /
+                           (apply str)))]
+    {:audio (do (println "We start with" (String. audio))
+                (if (some? (re-find #"the file is already in a common audio format" (String. audio)))
+                  (->> (String. audio)
+                       (#(do (println "We are at" %)
+                             %))
+                       reverse
+                       (drop-while #(not (= % \;)))
+                       (drop 1)
+                       reverse
+                       (apply str))
+                  (do (println "We give up with" (String. audio))
+                      (String. audio))))
+     :thumbnail (String. thumbnail)
+     :zip (do (println "Zip section with" thumbnail)
+              (->> thumbnail
+                   reverse
+                   (drop-while #(not (= % \.))) ;; drop extension
+                   reverse
+                   (apply str)
+                   (#(str % "zip"))))}))
 
 (defn octet-headers
   [filepath]
@@ -26,15 +58,38 @@
   [req]
   (let [data (json/json-from-request req)
         url (:url data)
-        filename (get-filename (yt-dlp/download-song "resources/%(title)s.%(ext)s" url))
-        filepath (str "resources/" filename)
-        response (server/respond
-                  (-> filepath
-                      io/file
-                      io/input-stream)
-                  :headers (octet-headers filepath))]
-    (sh/sh "rm"
-           "-f"
-           filepath)
+        filenames (get-output-names (yt-dlp/download-song "resources/%(title)s.%(ext)s" url))
+        path "resources/"]
 
-    response))
+    (try
+      (println "zip"
+               (str path (:zip filenames))
+               (str path (:audio filenames))
+               (str path (:thumbnail filenames)))
+      (sh/sh "zip"
+             (str path (:zip filenames))
+             (str path (:audio filenames))
+             (str path (:thumbnail filenames)))
+
+      (println "Now, respond.")
+
+      (let [response (server/respond
+                      (-> (str path (:zip filenames))
+                          io/file
+                          io/input-stream)
+                      :headers (octet-headers (str path (:zip filenames))))]
+        ;; (sh/sh "rm"
+        ;;        "-f"
+        ;;        (str path (:zip filenames))
+        ;;        (str path (:audio filenames))
+        ;;        (str path (:thumbnail filenames)))
+
+        response)
+
+      (catch Exception e
+        (println "An error ocurred: " (.getMessage e))
+        (sh/sh "rm"
+               "-f"
+               (str path (:zip filenames))
+               (str path (:audio filenames))
+               (str path (:thumbnail filenames)))))))
