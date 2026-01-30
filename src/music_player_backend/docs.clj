c276ef26b6a941400958bad8085de1bcbd4ce983
niljimeno <niliara@proton.me>
2026-01-30 11:40:43 +0100

    minichanges

--- src/music_player_backend/docs.clj
+++ src/music_player_backend/docs.clj
@@ -17,23 +17,31 @@
   {:info {:version "1.0.0"
           :title "Eilous"
           :description "Music player backend"}
-          ; :termsOfService "http://helloreverb.com/terms/"
-          ; :contact {:name "My API Team"
-          ;           :email "foo@example.com"
-          ;           :url "http://www.metosin.fi"}
-          ; :license {:name "Eclipse Public License"
-          ;           :url "http://www.eclipse.org/legal/epl-v10.html"}}
 
    :securityDefinitions {:jwt {:type "apiKey"
                                :name "Authorization"
-                               :in "header"}}
+                               :in "header"}
+                         :master-key {:type "apiKey"
+                                      :name "Authorization"
+                                      :in "header"}}
+
    :security [{:jwt []}]
 
-   :tags [{:name "user"
-           :description "User stuff"}]
-   :paths {"/login" {:get {:security []
-                           :parameters {:body Login}}}
-           "/song" {:get {:parameters {:query {:url s/Str}}}}
+   :tags [{:name "user"}
+          {:name "songs"}
+          {:name "playlists"}]
+   :paths {"/register" {:post {:security [{:master-key []}]
+                               :tags ["user"]
+                               :parameters {:body Login}}}
+           "/login" {:post {:security []
+                            :tags ["user"]
+                            :parameters {:body Login}}}
+           "/song" {:post {:tags ["songs"]
+                           :parameters {:body {:url s/Str}}}}
+           "/search" {:post {:tags ["songs"]
+                             :parameters {:body {:url s/Str}}}}
+           "/playlist" {:post {:tags ["playlist"]
+                               :parameters {:body {:url s/Str}}}}
            ; "/user/:id" {:post {:summary "User Api"
            ;                     :description "User Api description"
            ;                     :tags ["user"]
