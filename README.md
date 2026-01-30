386774fdbac12945d06122974f7bd02335ca151c
niljimeno <niliara@proton.me>
2026-01-30 12:24:49 +0100

    add swagger (complete)

--- README.md
+++ README.md
@@ -1,15 +1,2 @@
 # Music Player Backend
-Variables de entorno:
-- MASTER_KEY: clave de administrador para registrar usuarios
-- SECRET_KEY: clave para encriptar el token
-
-
-## Rutas
-### `/login`
-Espera: json
-```json
-{
-    { "username" : nombre_de_usuario },
-    { "password" : clave_de_usuario }
-}
-```
+documentacion en :8080/docs/index.html
--- src/music_player_backend/docs.clj
+++ src/music_player_backend/docs.clj
@@ -3,15 +3,34 @@
    [music-player-backend.server :as server]
    [ring.swagger.swagger2 :as rs]
    [clojure.data.json :as json]
-   [schema.core :as s]))
+   [schema.core :as s]
+   [music-player-backend.playlist :as playlist]))
 
-(s/defschema User {:id s/Str,
-                   :name s/Str
-                   :address {:street s/Str
-                             :city (s/enum :tre :hki)}})
+(s/defschema Playlist
+  {:id s/Int
+   :name s/Str})
+
+(s/defschema PlaylistPost
+  (dissoc Playlist :id))
+
+(s/defschema Delete
+  {:id s/Int})
+
+(s/defschema Song
+  {:id s/Int
+   :name s/Str
+   :url s/Str})
+
+(s/defschema SongPost
+  (dissoc Song :id))
+
 (s/defschema Login
   {:username s/Str,
    :password s/Str})
+
+(s/defschema UpdatePlaylist
+  {:id s/Int
+   :new [s/Int]})
 
 (def ^:private schema
   {:info {:version "1.0.0"
@@ -28,29 +47,36 @@
    :security [{:jwt []}]
 
    :tags [{:name "user"}
-          {:name "songs"}
-          {:name "playlists"}]
+          {:name "api"}
+          {:name "database"}]
    :paths {"/register" {:post {:security [{:master-key []}]
                                :tags ["user"]
                                :parameters {:body Login}}}
            "/login" {:post {:security []
                             :tags ["user"]
                             :parameters {:body Login}}}
-           "/song" {:post {:tags ["songs"]
+
+           "/song" {:post {:tags ["api"]
                            :parameters {:body {:url s/Str}}}}
-           "/search" {:post {:tags ["songs"]
-                             :parameters {:body {:url s/Str}}}}
-           "/playlist" {:post {:tags ["playlist"]
-                               :parameters {:body {:url s/Str}}}}
-           ; "/user/:id" {:post {:summary "User Api"
-           ;                     :description "User Api description"
-           ;                     :tags ["user"]
-           ;                     :parameters {:path {:id s/Str}
-           ;                                  :body User}
-           ;                     :responses {200 {:schema User
-           ;                                      :description "Found it!"}
-           ;                                 404 {:description "Ohnoes."}}}}}})
-           }})
+           "/search" {:post {:tags ["api"]
+                             :parameters {:body {:query s/Str}}}}
+
+           "/playlist" {:post {:tags ["database"]
+                               :parameters {:body PlaylistPost}}
+                        :put {:tags ["database"]
+                              :parameters {:body Playlist}}
+                        :delete {:tags ["database"]
+                                 :parameters {:body Delete}}}
+
+           "/track" {:post {:tags ["database"]
+                            :parameters {:body SongPost}}
+                     :put {:tags ["database"]
+                           :parameters {:body Song}}
+                     :delete {:tags ["database"]
+                              :parameters {:body Delete}}}
+
+           "/playlist/tracks" {:post {:tags ["database"]
+                                      :parameters {:body UpdatePlaylist}}}}})
 
 (def ^:private data
   (-> schema
