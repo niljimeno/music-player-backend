81e56242de959cc0a7a72fd159a3268e633345c0
niljimeno <niliara@proton.me>
2026-02-08 18:56:54 +0100

    add access control header

--- src/music_player_backend/server.clj
+++ src/music_player_backend/server.clj
@@ -4,7 +4,8 @@
 (defn respond
   "Template for HTTP responses"
   [body & {:keys [status headers] :or {status 200
-                                       headers {"content-type" "text/plain"}}}]
+                                       headers {"content-type" "text/plain"
+                                                "Access-Control-Allow-Origin" "*"}}}]
   {:status status
    :headers headers
    :body body})
